9c1fd228f03ce25de8c144eeed3937f69a3808e7
bhpm999 <mpolakov767@gmail.com>
2025-01-28 21:19:56 +0300

    created

--- src/main/java/com/example/repos/UserRepo.java
+++ src/main/java/com/example/repos/UserRepo.java
@@ -1,2 +1,10 @@
-package com.example.repos;public interface UserRepo {
+package com.example.repos;
+
+import com.example.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.nio.file.LinkOption;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
