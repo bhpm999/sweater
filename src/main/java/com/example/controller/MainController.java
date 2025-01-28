9047149538053085c56ec8906b9708acafa7d80b
bhpm999 <mpolakov767@gmail.com>
2025-01-28 21:15:29 +0300

    renamed

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.controller;
 
 import com.example.domain.Message;
 import com.example.repos.MessageRepo;
@@ -9,11 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
---
+++ src/main/java/com/example/repos/UserRepo.java
@@ -0,0 +1,2 @@
+package com.example.repos;public interface UserRepo {
+}
