4bace02ab154f859c54c324f0a0326148c60052f
bhpm999 <mpolakov767@gmail.com>
2025-01-30 22:57:20 +0300

    user linked to a message

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
 import com.example.domain.Message;
+import com.example.domain.User;
 import com.example.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +28,8 @@ public class MainController {
         return "main";
     }
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Model model){
-        Message message = new Message(text, tag);
+    public String add(@AuthenticationPrincipal User user, @RequestParam String text, @RequestParam String tag, Model model){
+        Message message = new Message(text, tag, user);
         messageRepo. save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
